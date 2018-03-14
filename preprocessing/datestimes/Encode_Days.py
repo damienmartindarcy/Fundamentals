ca72714710ce3e58a42706dc17c9e53dbfcdac96
Damien Darcy <damienmartindarcy@gmail.com>
2018-03-14 16:46:20 +0000

    Add files via upload

---
+++ preprocessing/datestimes/Encode_Days.py
@@ -0,0 +1,13 @@
+# Encode days of the week
+
+# Load library
+import pandas as pd
+ 
+# Create dates
+dates = pd.Series(pd.date_range('2/2/2002', periods=3, freq='M'))
+
+# View data
+dates
+
+# Show days of the week
+dates.dt.weekday_name
---
+++ preprocessing/datestimes/Rolling_Time_Window.py
@@ -0,0 +1,19 @@
+# Rolling time window
+
+# Load library
+import pandas as pd
+
+# Create datetimes
+time_index = pd.date_range('01/01/2010', periods=5, freq='M')
+
+# Create data frame, set index
+df = pd.DataFrame(index=time_index)
+
+# Create feature
+df['Stock_Price'] = [1,2,3,4,5]
+
+# Calculate rolling mean
+df.rolling(window=2).mean()
+
+# Identify max value in rolling time window
+df.rolling(window=2).max()
